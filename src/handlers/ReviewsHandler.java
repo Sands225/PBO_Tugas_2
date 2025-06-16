c39ad9574800ef423813e64bf6c15debdff0f0b5
Sands225 <sandikakadek2018@gmail.com>
2025-06-17 07:47:53 +0800

    updated SQL query

--- src/handlers/ReviewsHandler.java
+++ src/handlers/ReviewsHandler.java
@@ -9,13 +9,12 @@ import java.util.*;
 public class ReviewsHandler {
     public static List<Map<String, Object>> getReviewsByVillaId(int villaId) {
         List<Map<String, Object>> reviews = new ArrayList<>();
-        String sql = """
-            SELECT rv.*
-            FROM reviews rv
-            JOIN bookings b ON rv.booking = b.id
-            JOIN room_types r ON b.room_type = r.id
-            WHERE r.villa = ?
-        """;
+        String sql =
+            "SELECT rv.*" +
+            "FROM reviews rv" +
+            "JOIN bookings b ON rv.booking = b.id" +
+            "JOIN room_types r ON b.room_type = r.id" +
+            "WHERE r.villa = ?";
         try (Connection conn = Database.getConnection();
              PreparedStatement stmt = conn.prepareStatement(sql)) {
             stmt.setInt(1, villaId);
