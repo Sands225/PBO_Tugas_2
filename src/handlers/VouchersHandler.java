17852b450276abc927e8c70ce4c47e33fae00961
Sands225 <sandikakadek2018@gmail.com>
2025-06-18 06:11:29 +0800

    added getVoucherById method

--- src/handlers/VouchersHandler.java
+++ src/handlers/VouchersHandler.java
@@ -7,6 +7,7 @@ import java.sql.*;
 import java.util.*;
 
 public class VouchersHandler {
+    // GET
     public static List<Voucher> getAllVouchers() {
         List<Voucher> vouchers = new ArrayList<>();
         String sql = "SELECT * FROM vouchers";
@@ -33,4 +34,32 @@ public class VouchersHandler {
         return vouchers;
     }
 
+    public static Voucher getVoucherById(int id) {
+        String sql = "SELECT * FROM vouchers WHERE id = ?";
+        try (Connection conn = Database.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                return new Voucher(
+                        rs.getInt("id"),
+                        rs.getString("code"),
+                        rs.getString("description"),
+                        rs.getDouble("discount"),
+                        rs.getString("start_date"),
+                        rs.getString("end_date")
+                );
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    // POST
+
+
+    // PUT
+
+    // DELETE
 }
