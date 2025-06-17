0d88d13181e5ae5715b30aa1a6305a3183b109a9
Sands225 <sandikakadek2018@gmail.com>
2025-06-17 15:04:08 +0800

    added sendResponse method and updated GET vouchers endpoint

--- src/routes/VouchersRoutes.java
+++ src/routes/VouchersRoutes.java
@@ -1,17 +1,17 @@
 package routes;
 
+import handlers.VouchersHandler;
+import models.*;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.util.Map;
-import java.util.HashMap;
+
+import java.util.*;
 import java.io.OutputStream;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.util.Map;
-import java.util.HashMap;
+
 import java.io.IOException;
-import java.io.OutputStream;
 
 public class VouchersRoutes implements HttpHandler {
     @Override
@@ -22,7 +22,9 @@ public class VouchersRoutes implements HttpHandler {
 
         if (method.equals("GET")) {
             if (path.matches("/vouchers/?")) {
-                response.put("message", "List of vouchers");
+                List<Voucher> vouchers = VouchersHandler.getAllVouchers();
+                sendJsonResponse(exchange, vouchers);
+                return;
             } else if (path.matches("/vouchers/\\d+/?")) {
                 response.put("message", "Voucher detail");
             }
@@ -37,13 +39,27 @@ public class VouchersRoutes implements HttpHandler {
         sendResponse(exchange, response);
     }
 
-    private void sendResponse(HttpExchange exchange, Map<String, Object> data) throws IOException {
+    public void sendResponse(HttpExchange exchange, Map<String, Object> data) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
         String json = mapper.writeValueAsString(data);
+
         exchange.getResponseHeaders().add("Content-Type", "application/json");
         exchange.sendResponseHeaders(200, json.getBytes().length);
         OutputStream os = exchange.getResponseBody();
+
         os.write(json.getBytes());
         os.close();
+    }
+
+    private void sendJsonResponse(HttpExchange exchange, Object data) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(data);
+
+        exchange.getResponseHeaders().add("Content-Type", "application/json");
+        exchange.sendResponseHeaders(200, json.getBytes().length);
+
+        try (OutputStream os = exchange.getResponseBody()) {
+            os.write(json.getBytes());
+        }
     }
 }
