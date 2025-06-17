1783c1ecad3533cc6330813b69037a96c7d39512
Sands225 <sandikakadek2018@gmail.com>
2025-06-17 15:04:52 +0800

    added getAllVouchers method

--- src/handlers/VouchersHandler.java
+++ src/handlers/VouchersHandler.java
@@ -7,5 +7,30 @@ import java.sql.*;
 import java.util.*;
 
 public class VouchersHandler {
+    public static List<Voucher> getAllVouchers() {
+        List<Voucher> vouchers = new ArrayList<>();
+        String sql = "SELECT * FROM vouchers";
+
+        try (Connection conn = Database.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+
+            while (rs.next()) {
+                Voucher voucher = new Voucher(
+                        rs.getInt("id"),
+                        rs.getString("code"),
+                        rs.getString("description"),
+                        rs.getDouble("discount"),
+                        rs.getString("start_date"),
+                        rs.getString("end_date")
+                );
+                vouchers.add(voucher);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return vouchers;
+    }
 
 }
