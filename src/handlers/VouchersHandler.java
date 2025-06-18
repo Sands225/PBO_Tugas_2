2ffb161e4befad1e457d1f5e3bdb11da728eccbc
Putra <puterayasa180@gmail.com>
2025-06-18 14:02:19 +0800

    Update Voucher Handler

--- src/handlers/VouchersHandler.java
+++ src/handlers/VouchersHandler.java
@@ -58,47 +58,12 @@ public class VouchersHandler {
 
     // POST
     // ERI
-    public static boolean insertVoucher(Voucher voucher) {
-        String sql = "INSERT INTO vouchers (code, description, discount, start_date, end_date) VALUES (?, ?, ?, ?, ?)";
 
-        try (Connection conn = Database.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            pstmt.setString(1, voucher.getCode());
-            pstmt.setString(2, voucher.getDescription());
-            pstmt.setDouble(3, voucher.getDiscount());
-            pstmt.setString(4, voucher.getStart_date()); // Format: "YYYY-MM-DD hh:mm:ss"
-            pstmt.setString(5, voucher.getEnd_date());
-
-            int affectedRows = pstmt.executeUpdate();
-            return affectedRows > 0;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
     // ERI
 
     // PUT
     // ARYA
-    public static boolean updateVoucher(Voucher voucher) {
-        String sql = "UPDATE vouchers SET code = ?, description = ?, discount = ?, start_date = ?, end_date = ? WHERE id = ?";
 
-        try (Connection conn = Database.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-
-            pstmt.setString(1, voucher.getCode());
-            pstmt.setString(2, voucher.getDescription());
-            pstmt.setDouble(3, voucher.getDiscount());
-            pstmt.setString(4, voucher.getStart_date()); // Format: "YYYY-MM-DD hh:mm:ss"
-            pstmt.setString(5, voucher.getEnd_date());
-            pstmt.setInt(6, voucher.getId());
-
-            return pstmt.executeUpdate() > 0;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
     // ARYA
 
     // DELETE
