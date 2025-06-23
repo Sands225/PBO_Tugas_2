a46c387481690f6993021b35fc8bbb33a09451ac
Putra <puterayasa180@gmail.com>
2025-06-23 16:07:01 +0800

    Update Voucher Routes

--- src/routes/VouchersRoutes.java
+++ src/routes/VouchersRoutes.java
@@ -2,20 +2,19 @@ package routes;
 
 import handlers.VillasHandler;
 import handlers.VouchersHandler;
-import models.*;
+import models.Voucher;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.io.InputStream;
-import java.util.*;
-import java.io.OutputStream;
-
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.IOException;
+import java.util.*;
 
 public class VouchersRoutes implements HttpHandler {
+
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         String method = exchange.getRequestMethod();
@@ -34,15 +33,15 @@ public class VouchersRoutes implements HttpHandler {
 
                 boolean success = VouchersHandler.insertVoucher(voucher);
                 if (success) {
-                    response.put("message", "Voucher created successfully");
+                    response.put(Utils.MESSAGE_KEY, Utils.VOUCHER_CREATED_SUCCESS);
                 } else {
-                    response.put("error", "Failed to create voucher");
+                    response.put(Utils.ERROR_KEY, Utils.VOUCHER_CREATE_FAILED);
                 }
                 sendResponse(exchange, response);
                 return;
             }
         } else if (method.equals("POST") && path.matches("/vouchers/?")) {
-            response.put("message", "Create voucher");
+            response.put(Utils.MESSAGE_KEY, "Create voucher");
         } else if (method.equals("PUT") && path.matches("/vouchers/\\d+/?")) {
             int voucherId = Integer.parseInt(path.split("/")[2]);
             InputStream is = exchange.getRequestBody();
@@ -51,9 +50,9 @@ public class VouchersRoutes implements HttpHandler {
 
             boolean success = VouchersHandler.updateVoucher(voucher);
             if (success) {
-                response.put("message", "Voucher updated successfully");
+                response.put(Utils.MESSAGE_KEY, Utils.VOUCHER_UPDATED_SUCCESS);
             } else {
-                response.put("error", "Failed to update voucher");
+                response.put(Utils.ERROR_KEY, Utils.VOUCHER_UPDATE_FAILED);
             }
             sendResponse(exchange, response);
             return;
@@ -62,9 +61,9 @@ public class VouchersRoutes implements HttpHandler {
             boolean success = VillasHandler.deleteVillaById(voucherId);
 
             if (success) {
-                response.put("message", "Voucher deleted successfully");
+                response.put(Utils.MESSAGE_KEY, Utils.VOUCHER_DELETED_SUCCESS);
             } else {
-                response.put("error", "Failed to delete voucher");
+                response.put(Utils.ERROR_KEY, Utils.VOUCHER_DELETE_FAILED);
             }
             sendResponse(exchange, response);
             return;
@@ -80,7 +79,6 @@ public class VouchersRoutes implements HttpHandler {
         exchange.getResponseHeaders().add("Content-Type", "application/json");
         exchange.sendResponseHeaders(200, json.getBytes().length);
         OutputStream os = exchange.getResponseBody();
-
         os.write(json.getBytes());
         os.close();
     }
@@ -91,7 +89,6 @@ public class VouchersRoutes implements HttpHandler {
 
         exchange.getResponseHeaders().add("Content-Type", "application/json");
         exchange.sendResponseHeaders(200, json.getBytes().length);
-
         try (OutputStream os = exchange.getResponseBody()) {
             os.write(json.getBytes());
         }
