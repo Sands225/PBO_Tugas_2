96a7195bc5bffb978d0f38cc85f257277fd8b16d
Sands225 <sandikakadek2018@gmail.com>
2025-06-24 22:17:03 +0800

    updated class name from VillaNotFound to be NotFoundException

--- src/exceptions/NotFoundException.java
+++ src/exceptions/NotFoundException.java
@@ -1,7 +1,7 @@
 package exceptions;
 
-public class VillaNotFoundException extends RuntimeException {
-    public VillaNotFoundException(String message) {
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
         super(message);
     }
 }
